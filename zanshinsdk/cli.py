6a08f8515325da0490875d40f87ccb286d565e78
Leonardo <lsantos@buus.com.br>
2021-06-09 12:28:16 -0300

    Removing page and size parameters; Adding interator in the CLI to fetch all pages in a single return;

--- zanshinsdk/cli.py
+++ zanshinsdk/cli.py
@@ -39,8 +39,6 @@ def list_alerts(organization_id: str,
                     None, help="Current Alert Severity. Options are [\"CRITICAL\" \"HIGH\" \"MEDIUM\" \"LOW\" \"INFO\"]"),
                 tag: str = typer.Argument(
                     None, help="Alerts with given Tags"),
-                page: int = 1,
-                size: int = 1000,
                 profile: str = typer.Option("default",
                                             help="Configuration file section to use for credentials and other settings")):
     """
@@ -60,25 +58,35 @@ def list_alerts(organization_id: str,
         Alert Severity, can be CRITICAL, HIGH, MEDIUM, LOW, or INFO
     tag : str, optional
         Alerts that contain given tag
-    page : int, optional, default 1
-        Page number of the query
-    size : int, optional, default 1000
-        Number of alerts returned by each page
     profile : string, optional, default \"default\"
         Profile defined in the config file
     """
     if not organization_id:
         return
     conn = Connection(profile=profile)
-    typer.echo(dumps(conn.list_alerts(
-        organization_id=organization_id,
-        scan_target_id=scan_target_id,
-        following_id=following_id,
-        state=state,
-        severity=severity,
-        tag=tag,
-        page=page,
-        size=size), indent=4))
+
+    page = 1
+    size = 1000
+    data = []
+    while True:
+        res = conn.list_alerts(
+            organization_id=organization_id,
+            scan_target_id=scan_target_id,
+            following_id=following_id,
+            state=state,
+            severity=severity,
+            tag=tag,
+            page=page,
+            size=size)
+        page += 1
+        for alert in res['data']:
+            data.append(alert)
+
+        if len(data) >= res['total']:
+            break
+
+    typer.echo(dumps({'total': res['total'], 'data': data}, indent=4))
+
 
 if __name__ == "__main__":
     app()
