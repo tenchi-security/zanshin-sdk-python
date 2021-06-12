32f8bda672f5d32e0ce154dda9b6b9b4d05bb24f
Alexandre Sieira <alexandre.sieira@gmail.com>
2021-06-12 17:29:40 -0300

    First PyPI release
    * Added README.
    * Updated makefile to properly release sdist to PyPI and PyPI-test.

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
+sdist:
+	rm -f dist/*
+	python setup.py sdist
+
 README.rst: README.md
 	pandoc --from=gfm --to=rst -o README.rst README.md
 
-pypi: README.rst
+pypi: README.rst sdist
 	python setup.py clean
 	twine upload --repository pypi dist/*
 
-pypitest: README.rst
+pypitest: README.rst sdist
 	python setup.py clean
 	twine upload --repository pypitest dist/*
--- setup.py
+++ setup.py
@@ -1,33 +1,23 @@
-import io
+from pathlib import Path
 
 from setuptools import setup
+
 from zanshinsdk import __version__
-
-
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
-
 
 setup(
     name='zanshinsdk',
     description='Python SDK to access the Tenchi Security Zanshin API v1',
-    long_description=read('README.rst'),
+    long_description=Path('README.rst').read_text(),
     author='Tenchi Security',
     author_email='contact@tenchisecurity.com',
     version=__version__,
     url='https://github.com/tenchi-security/zanshin-sdk-python',
     license='Apache Software License',
-    install_requires=['typer>=0.3.2','requests>=2.25,<3', 'types-requests>=0.1.8,<1', 'prettytable>=2.1,<3'],
+    install_requires=['typer>=0.3.2', 'requests>=2.25,<3', 'types-requests>=0.1.8,<1', 'prettytable>=2.1,<3'],
     tests_require=['pytest>=6.2,<7', 'pandoc'],
     setup_requires=['pytest-runner>=5.3,<6'],
     packages=['zanshinsdk'],
-    #include_package_data=True,
+    # include_package_data=True,
     platforms='any',
     classifiers=[
         'Programming Language :: Python',
