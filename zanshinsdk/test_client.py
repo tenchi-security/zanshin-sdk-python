56b16cd1b3b1ba2dba329d5f87e9cb984a87d30b
Eduardo Pinheiro <edpin@tenchisecurity.com>
2021-12-15 13:57:17 -0800

    simplifies test

--- zanshinsdk/test_client.py
+++ zanshinsdk/test_client.py
@@ -5,104 +5,84 @@ import zanshinsdk
 
 class TestClient(unittest.TestCase):
     @patch("zanshinsdk.Client._request")
-    def test_get_me(self, _):
-        cli = zanshinsdk.Client()
-        cli.get_me()
+    def setUp(self, request):
+        self.cli = zanshinsdk.Client(profile=None, api_key="abc")
+        self.cli._request = request
 
-        cli._request.assert_called_once_with('GET', '/me')
 
-    @patch("zanshinsdk.Client._request")
-    def test_iter_invites(self, _):
-        cli = zanshinsdk.Client()
+    def test_get_me(self):
+        self.cli.get_me()
 
+        self.cli._request.assert_called_once_with('GET', '/me')
+
+    def test_iter_invites(self):
         try:
-            next(cli.iter_invites())
+            next(self.cli.iter_invites())
         except StopIteration:
             pass
 
-        cli._request.assert_called_once_with('GET', '/me/invites')
+        self.cli._request.assert_called_once_with('GET', '/me/invites')
 
-    @patch("zanshinsdk.Client._request")
-    def test_get_invite(self, _):
-        cli = zanshinsdk.Client()
-        cli.get_invite("a72f4225-43e9-4922-b6b7-8b0620bdb1c0")
+    def test_get_invite(self):
+        self.cli.get_invite("a72f4225-43e9-4922-b6b7-8b0620bdb1c0")
 
-        cli._request.assert_called_once_with('GET', '/me/invites/a72f4225-43e9-4922-b6b7-8b0620bdb1c0')
+        self.cli._request.assert_called_once_with('GET', '/me/invites/a72f4225-43e9-4922-b6b7-8b0620bdb1c0')
 
-    @patch("zanshinsdk.Client._request")
-    def test_accept_invite(self, _):
-        cli = zanshinsdk.Client()
-        cli.accept_invite("a72f4225-43e9-4922-b6b7-8b0620bdb1c0")
+    def test_accept_invite(self):
+        self.cli.accept_invite("a72f4225-43e9-4922-b6b7-8b0620bdb1c0")
 
-        cli._request.assert_called_once_with('POST', '/me/invites/a72f4225-43e9-4922-b6b7-8b0620bdb1c0/accept')
+        self.cli._request.assert_called_once_with('POST', '/me/invites/a72f4225-43e9-4922-b6b7-8b0620bdb1c0/accept')
 
 
-    @patch("zanshinsdk.Client._request")
-    def test_iter_api_keys(self, _):
-        cli = zanshinsdk.Client()
-
+    def test_iter_api_keys(self):
         try:
-            next(cli.iter_api_keys())
+            next(self.cli.iter_api_keys())
         except StopIteration:
             pass
 
-        cli._request.assert_called_once_with('GET', '/me/apikeys')
+        self.cli._request.assert_called_once_with('GET', '/me/apikeys')
 
-    @patch("zanshinsdk.Client._request")
-    def test_create_api_key(self, _):
-        cli = zanshinsdk.Client()
-        cli.create_api_key("my key")
+    def test_create_api_key(self):
+        self.cli.create_api_key("my key")
 
-        cli._request.assert_called_once_with('POST', '/me/apikeys', body={"name": "my key"})
+        self.cli._request.assert_called_once_with('POST', '/me/apikeys', body={"name": "my key"})
 
+    def test_delete_api_key(self):
+        self.cli.delete_api_key("a72f4225-43e9-4922-b6b7-8b0620bdb1c0")
 
-    @patch("zanshinsdk.Client._request")
-    def test_delete_api_key(self, _):
-        cli = zanshinsdk.Client()
-        cli.delete_api_key("a72f4225-43e9-4922-b6b7-8b0620bdb1c0")
+        self.cli._request.assert_called_once_with('DELETE', '/me/apikeys/a72f4225-43e9-4922-b6b7-8b0620bdb1c0')
 
-        cli._request.assert_called_once_with('DELETE', '/me/apikeys/a72f4225-43e9-4922-b6b7-8b0620bdb1c0')
-
-    @patch("zanshinsdk.Client._request")
-    def test_iter_organization_members(self, _):
-        cli = zanshinsdk.Client()
-
+    def test_iter_organization_members(self):
         try:
-            next(cli.iter_organization_members("a72f4225-43e9-4922-b6b7-8b0620bdb1c0"))
+            next(self.cli.iter_organization_members("a72f4225-43e9-4922-b6b7-8b0620bdb1c0"))
         except StopIteration:
             pass
 
-        cli._request.assert_called_once_with('GET', '/organizations/a72f4225-43e9-4922-b6b7-8b0620bdb1c0/members')
+        self.cli._request.assert_called_once_with('GET', '/organizations/a72f4225-43e9-4922-b6b7-8b0620bdb1c0/members')
 
-    @patch("zanshinsdk.Client._request")
-    def test_get_organization_members(self, _):
-        cli = zanshinsdk.Client()
-        cli.get_organization_member('a72f4225-43e9-4922-b6b7-8b0620bdb1c0', 'b72f4225-43e1-1922-b6b7-8b0620bdb1d1')
+    def test_get_organization_members(self):
+        self.cli.get_organization_member('a72f4225-43e9-4922-b6b7-8b0620bdb1c0', 'b72f4225-43e1-1922-b6b7-8b0620bdb1d1')
 
-        cli._request.assert_called_once_with('GET', '/organizations/a72f4225-43e9-4922-b6b7-8b0620bdb1c0/members/b72f4225-43e1-1922-b6b7-8b0620bdb1d1')
+        self.cli._request.assert_called_once_with('GET', '/organizations/a72f4225-43e9-4922-b6b7-8b0620bdb1c0/members/b72f4225-43e1-1922-b6b7-8b0620bdb1d1')
 
-    @patch("zanshinsdk.Client._request")
-    def test_update_organization_members(self, _):
-        cli = zanshinsdk.Client()
-
+    def test_update_organization_members(self):
         #TODO(edpin): figure out why importing Roles is not working.
-        cli.update_organization_member(
+        self.cli.update_organization_member(
             'a72f4225-43e9-4922-b6b7-8b0620bdb1c0',
             'b72f4225-43e1-1922-b6b7-8b0620bdb1d1',
             ['ADMIN'])
 
-        cli._request.assert_called_once_with(
+        self.cli._request.assert_called_once_with(
             'PUT',
             '/organizations/a72f4225-43e9-4922-b6b7-8b0620bdb1c0/members/b72f4225-43e1-1922-b6b7-8b0620bdb1d1',
             body = {"roles": ['ADMIN']})
 
-    @patch("zanshinsdk.Client._request")
-    def test_delete_organization_members(self, _):
+    def test_delete_organization_members(self):
         cli = zanshinsdk.Client()
-        cli.delete_organization_member(
+        self.cli.delete_organization_member(
             'a72f4225-43e9-4922-b6b7-8b0620bdb1c0',
             'b72f4225-43e1-1922-b6b7-8b0620bdb1d1')
 
-        cli._request.assert_called_once_with(
+        self.cli._request.assert_called_once_with(
             'DELETE',
             '/organizations/a72f4225-43e9-4922-b6b7-8b0620bdb1c0/members/b72f4225-43e1-1922-b6b7-8b0620bdb1d1')
