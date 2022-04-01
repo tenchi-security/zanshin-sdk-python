367b394b2e1cb61f44158a3a8fcc5019869bd834
lviveiros <leonardoviveirossantos@gmail.com>
2022-04-01 10:24:44 -0300

    Adding __version__ to __init__ of zanshinsdk module

--- zanshinsdk/__init__.py
+++ zanshinsdk/__init__.py
@@ -3,6 +3,7 @@ from zanshinsdk.client import Client, AlertState, AlertSeverity, ScanTargetKind,
 from zanshinsdk.iterator import AbstractPersistentAlertsIterator, PersistenceEntry
 from zanshinsdk.alerts_history import FilePersistentAlertsIterator
 from zanshinsdk.following_alerts_history import FilePersistentFollowingAlertsIterator
+from zanshinsdk.version import __version__
 
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
