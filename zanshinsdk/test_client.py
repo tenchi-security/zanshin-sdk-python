e623ea3ca7a6809e725c3bf3efe711e67a6e6410
Isabella Fagioli <ifagioli@tenchisecurity.com>
2022-12-27 15:32:41 -0300

    add feat to create a scan target group

--- zanshinsdk/test_client.py
+++ zanshinsdk/test_client.py
@@ -1113,6 +1113,17 @@ class TestClient(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.sdk.get_organization_scan_target_group("foo", scan_target_group_id)
 
+    def test_create_organization_scan_target(self):
+        organization_id = "822f4225-43e9-4922-b6b8-8b0620bdb1e3"
+        kind = zanshinsdk.ScanTargetKind.AWS
+        name = "ScanTargetTest"
+
+        self.sdk.create_organization_scan_target(organization_id, kind, name)
+
+        self.sdk._request.assert_called_once_with(
+            "POST", f"/organizations/{organization_id}/scantargetgroups",
+            body={"name": name, "kind": kind}
+        )
 
     def test_iter_scan_target_group_compartments(self):
         organization_id = "822f4225-43e9-4922-b6b8-8b0620bdb1e3"
