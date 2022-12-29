bb7a3c4d938af268d28c15bfad82deba695b25f9
Isafagioli-Tenchi <120130136+Isafagioli-Tenchi@users.noreply.github.com>
2022-12-29 11:22:34 -0300

    add feat to insert a scan target group credential (#84)

--- zanshinsdk/__init__.py
+++ zanshinsdk/__init__.py
@@ -1,5 +1,5 @@
 from zanshinsdk.client import Client, AlertState, AlertSeverity, ScanTargetKind, ScanTargetSchedule, Languages, \
-    AlertsOrderOpts, SortOpts, ScanTargetAWS, ScanTargetAZURE, ScanTargetGCP, ScanTargetHUAWEI, ScanTargetDOMAIN, Roles, validate_uuid
+    AlertsOrderOpts, SortOpts, ScanTargetAWS, ScanTargetAZURE, ScanTargetGCP, ScanTargetHUAWEI, ScanTargetDOMAIN, ScanTargetGroupCredentialListORACLE, Roles, validate_uuid
 from zanshinsdk.iterator import AbstractPersistentAlertsIterator, PersistenceEntry
 from zanshinsdk.alerts_history import FilePersistentAlertsIterator
 from zanshinsdk.following_alerts_history import FilePersistentFollowingAlertsIterator
