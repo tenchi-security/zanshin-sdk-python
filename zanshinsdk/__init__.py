91e5843b6614bb4cbf03943030ce6fe6338bfa00
Romulo Gatto <rgatto@tenchisecurity.com>
2023-02-10 16:24:21 -0300

    fix: circular reference

--- zanshinsdk/__init__.py
+++ zanshinsdk/__init__.py
@@ -1,6 +1,5 @@
 import logging
 
-from zanshinsdk.alerts_history import FilePersistentAlertsIterator
 from zanshinsdk.client import (
     AlertSeverity,
     AlertsOrderOpts,
@@ -22,5 +21,7 @@ from zanshinsdk.client import (
 from zanshinsdk.following_alerts_history import FilePersistentFollowingAlertsIterator
 from zanshinsdk.iterator import AbstractPersistentAlertsIterator, PersistenceEntry
 from zanshinsdk.version import __version__
+
+from zanshinsdk.alerts_history import FilePersistentAlertsIterator  # isort:skip
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
