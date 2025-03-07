d1ffc1db0cb1f9321d126e353926b293a438d39e
Eduardo Palhares <epalhares@tenchisecurity.com>
2025-03-07 14:01:46 -0300

    add BaseAlertFilter support to trouped alerts commands (#157)
    * add base alert filter validator
    
    * add validator to iter grouped following alerts
    
    * update grouped own alerts method
    
    * reverting connection update

--- zanshinsdk/common/validators.py
+++ zanshinsdk/common/validators.py
@@ -1,6 +1,15 @@
+from copy import deepcopy
 from datetime import datetime
-from typing import Optional, Union
+from typing import Iterable, Optional, Union
 from uuid import UUID
+
+from zanshinsdk.common.enums import (
+    AlertSeverity,
+    AlertsOrderOpts,
+    AlertState,
+    Languages,
+    SortOpts,
+)
 
 
 def validate_int(
@@ -64,3 +73,69 @@ def validate_date(
         )
 
     raise TypeError(f"{repr(date)} is not a valid date type")
+
+
+def validate_base_alert_filter(
+    body: dict,
+    rules: Optional[Iterable[str]] = None,
+    states: Optional[Iterable[AlertState]] = None,
+    severities: Optional[Iterable[AlertSeverity]] = None,
+    search: Optional[str] = None,
+    lang: Optional[Languages] = None,
+    opened_at_start: Optional[str] = None,
+    opened_at_end: Optional[str] = None,
+    resolved_at_start: Optional[str] = None,
+    resolved_at_end: Optional[str] = None,
+    created_at_start: Optional[str] = None,
+    created_at_end: Optional[str] = None,
+    updated_at_start: Optional[str] = None,
+    updated_at_end: Optional[str] = None,
+    order: Optional[AlertsOrderOpts] = None,
+    sort: Optional[SortOpts] = None,
+) -> dict:
+    cloned_body = deepcopy(body)
+    if rules:
+        cloned_body["rules"] = [validate_class(rule, str) for rule in rules]
+    if states:
+        if isinstance(states, str) or isinstance(states, AlertState):
+            states = [states]
+        validate_class(states, Iterable)
+        cloned_body["states"] = [
+            validate_class(state, AlertState).value for state in states
+        ]
+    if severities:
+        if isinstance(severities, str):
+            severities = [severities]
+        validate_class(severities, Iterable)
+        cloned_body["severities"] = [
+            validate_class(severity, AlertSeverity).value for severity in severities
+        ]
+    if lang:
+        validate_class(lang, Languages)
+        cloned_body["lang"] = lang.value
+    if opened_at_start:
+        cloned_body["openedAtStart"] = validate_date(opened_at_start).isoformat()
+    if opened_at_end:
+        cloned_body["openedAtEnd"] = validate_date(opened_at_end).isoformat()
+    if resolved_at_start:
+        cloned_body["resolvedAtStart"] = validate_date(resolved_at_start).isoformat()
+    if resolved_at_end:
+        cloned_body["resolvedAtEnd"] = validate_date(resolved_at_end).isoformat()
+    if created_at_start:
+        cloned_body["createdAtStart"] = validate_date(created_at_start).isoformat()
+    if created_at_end:
+        cloned_body["createdAtEnd"] = validate_date(created_at_end).isoformat()
+    if updated_at_start:
+        cloned_body["updatedAtStart"] = validate_date(updated_at_start).isoformat()
+    if updated_at_end:
+        cloned_body["updatedAtEnd"] = validate_date(updated_at_end).isoformat()
+    if order:
+        validate_class(order, AlertsOrderOpts)
+        cloned_body["order"] = order.value
+    if search:
+        validate_class(search, str)
+        cloned_body["search"] = search
+    if sort:
+        validate_class(sort, SortOpts)
+        cloned_body["sort"] = sort.value
+    return cloned_body
